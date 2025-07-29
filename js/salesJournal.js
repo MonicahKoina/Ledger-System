1565e58ed2f70267df9c9e4bf0fe04f83dc4f82b
Monicah Koina <monicahkoina1@gmail.com>
2025-07-29 21:23:42 +0300

    save credit sale to local storage

---
+++ js/salesJournal.js
@@ -0,0 +1,27 @@
+let form = document.getElementById("form");
+let invoice = document.getElementById("invoice")
+let customerName = document.getElementById("customerName")
+let date = document.getElementById("date")
+let itemName = document.getElementById("itemName")
+let itemQuantity = document.getElementById("itemQuantity")
+let quantityMeasure = document.getElementById("quantityMeasure")
+function generateInvoice() {
+    return invoice.value = "SALE-" + crypto.randomUUID().slice(0, 8).toUpperCase()
+}
+generateInvoice()
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let sales = {
+        invoice: invoice.value,
+        date: date.value,
+        customerName: customerName.value,
+        itemName: itemName.value,
+        itemQuantity: itemQuantity.value,
+        quantityMeasure: quantityMeasure.value
+    }
+    let savedSales = JSON.parse(localStorage.getItem("savedPurchases")) || []
+    savedSales.push(sales)
+    localStorage.setItem("savedSales", JSON.stringify(savedSales))
+    form.reset()
+    generateInvoice()
+})
